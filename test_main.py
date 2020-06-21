383971ca06b3d40182552078d6d56a0cbd8b02cc
Ali Jafer <alijaferalamer@gmail.com>
2020-06-21 17:21:22 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
+SECRET = 'Q3kRLpt9jkstVWG'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
@@ -26,7 +26,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
-    assert False
+    
 
     
 def test_auth(client):
@@ -39,4 +39,4 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
-    assert False
+    
